b3f77ebea958c9fe08608cdb8ec971dc0fd144d5
MaHeR <45547826+maheralayoubi@users.noreply.github.com>
2019-12-19 23:23:50 +0900

    Adding event listener

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,3 +4,8 @@ const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-items');
+
+
+// Set Initial State Of Menu
+
+let showMenu = false;
