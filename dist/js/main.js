3af52160482ccc3f5abde58e40d3ca3f8e592bf1
MaHeR <45547826+maheralayoubi@users.noreply.github.com>
2019-12-20 23:30:20 +0900

    Else statment

--- dist/js/main.js
+++ dist/js/main.js
@@ -23,6 +23,13 @@ function toggleMenu () {
         // Set Menu State
         showMenu = true;
     } else {
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
 
+        // Set Menu State
+        showMenu = false;
     }
 }
